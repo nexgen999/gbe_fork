f6e20d95af3c062c9be94c34f0e51de2436d7ba4
otavepto <153766569+otavepto@users.noreply.github.com>
2023-12-24 06:42:36 +0200

    added original source

--- steamclient_loader/ColdClientLoader.cpp
+++ steamclient_loader/ColdClientLoader.cpp
@@ -1,4 +1,5 @@
 // My own modified version of ColdClientLoader originally written by Rat431
+// https://github.com/Rat431/ColdAPI_Steam/tree/master/src/ColdClientLoader
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
