d5035d3dbd96bee21a5e40812e6865d3ba20768d
otavepto <153766569+otavepto@users.noreply.github.com>
2023-12-26 19:25:53 +0200

    dynamic thread count in packager script for gen emu config

--- tools/generate_emu_config/package_win.bat
+++ tools/generate_emu_config/package_win.bat
@@ -11,8 +11,14 @@ set "out_dir=bin\package\win"
 
 set /a MEM_PERCENT=90
 set /a DICT_SIZE_MB=384
-set /a THREAD_COUNT=13
 set "packager=..\..\third-party\deps\win\7za\7za.exe"
+
+:: use 70%
+if defined NUMBER_OF_PROCESSORS (
+  set /a THREAD_COUNT=NUMBER_OF_PROCESSORS*70/100
+) else (
+  set /a THREAD_COUNT=2
+)
 
 if not exist "%packager%" (
   1>&2 echo "[X] packager app wasn't found"
