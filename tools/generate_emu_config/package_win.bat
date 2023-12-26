7dc92f0bdf1fb936f4fe32ce1a29e866dda12ee5
otavepto <153766569+otavepto@users.noreply.github.com>
2023-12-26 19:12:19 +0200

    fixed packager app path in gen emu config

--- tools/generate_emu_config/package_win.bat
+++ tools/generate_emu_config/package_win.bat
@@ -12,7 +12,7 @@ set "out_dir=bin\package\win"
 set /a MEM_PERCENT=90
 set /a DICT_SIZE_MB=384
 set /a THREAD_COUNT=13
-set "packager=..\third-party\deps\win\7za-win\7za.exe"
+set "packager=..\..\third-party\deps\win\7za\7za.exe"
 
 if not exist "%packager%" (
   1>&2 echo "[X] packager app wasn't found"
