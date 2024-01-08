0f002f7ef95c1119ee940f5eb609f26b67de2553
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2024-01-08 21:37:45 +0100

    Update steam_ugc.h
    forgot this one

--- dll/dll/steam_ugc.h
+++ dll/dll/steam_ugc.h
@@ -98,7 +98,7 @@ void set_details(PublishedFileId_t id, SteamUGCDetails_t *pDetails)
             pDetails->m_flScore = settings->getMod(id).score;
             // implement something like:
             pDetails->m_hFile = ugc_file_handle;
-            pDetails->m_hPreviewFile = ugc_prev_handle:
+            pDetails->m_hPreviewFile = ugc_prev_handle;
             //pDetails->m_unNumChildren = settings->getMod(id).numChildren;
         } else {
             pDetails->m_nPublishedFileId = id;
